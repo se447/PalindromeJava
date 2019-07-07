dc801bf9f1ae21704db64721c9a4f33388a00abd
Saad E <21321555+se447@users.noreply.github.com>
2019-07-07 14:26:31 -0400

    Created method that reverses a string

--- src/Palindromi.java
+++ src/Palindromi.java
@@ -1,5 +1,38 @@
+/*Create the method palindrome that checks if a string is a palindrome (reads the same forward and backward).*/
 import java.util.Scanner;
 
+public class Palindromi {
+    
+    //method that reverses a string
+    public static String reverseString (String text) {
+        String reversed = "";
+        
+        int count = text.length() - 1;
+        
+        while (count >= 0) {
+            reversed += text.charAt(count);
+            count--;
+        }
+        
+        return reversed;
+    }
+    /*
+    public static boolean palindrome(String text) {
+    
+    }
+    */
+    
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        
+        System.out.println(reverseString(text));
+    }
+}
+
+/*
 public class Palindromi {
 
     public static boolean palindrome(String text) {
@@ -8,10 +41,7 @@ public class Palindromi {
     }
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        
-        System.out.println("Type a text: ");
-        String text = reader.nextLine();    
+         
         if (palindrome(text)) {
             System.out.println("The text is a palindrome!");
         } else {
@@ -19,3 +49,4 @@ public class Palindromi {
         }
     }
 }
+*/
